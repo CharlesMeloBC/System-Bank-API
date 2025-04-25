c3eb4d4f75c61fe10b92aa1758f67d3b07e81d18
Charles <charles.melo145@gmail.com>
2025-04-24 23:23:57 -0300

    fix: mudanca de arquitetura nas pastas

---
+++ AccountBank/Migrations/20250417030547_CreateTablesBatch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountBank.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTablesBatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Branch = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    NumberAccount = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TypeAccount = table.Column<int>(type: "int", nullable: false),
+                    HolderName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    HolderEmail = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    HolderDocuments = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HolderType = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CodeBank = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    BankName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Balances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    AvaliableAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    BlokedAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Balances", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Balances_Accounts_Id",
+                        column: x => x.Id,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Balances");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
